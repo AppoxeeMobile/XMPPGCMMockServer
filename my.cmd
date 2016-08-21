02186d5cd3522b6109c40388aca9b0b1c3445170
evyatarkafkafi <Evyatar.Kafkafi@teradata.com>
2016-08-21 11:12:55 +0300

    mock server code with few modifications

---
+++ my.cmd
@@ -0,0 +1,2 @@
+cd C:\Work\teradata\mock1\gcmccsmock-master
+java -cp target/mockgcmxmppserver-1.0.0-SNAPSHOT-jar-with-dependencies.jar -DconfigFile=C:/Work/teradata/mock1/gcmccsmock-master/config1.properties com.jql.gcmccsmock.MockGCMXmppServer
